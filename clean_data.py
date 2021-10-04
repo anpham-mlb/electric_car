38905b8cc5df491931bae89b5850c8e17b6c1490
An Pham <mikeloongboong@gmail.com>
2021-10-04 12:53:24 +1100

    push_data

--- clean_data.py
+++ clean_data.py
@@ -8,7 +8,14 @@ df["Range"] = df["Range"].apply(lambda i: i.split(" km")[0])
 df["Efficiency"] = df["Efficiency"].apply(lambda i: i.split(" Wh/km")[0])
 df["FastChargeSpeed"] = df["FastChargeSpeed"].apply(lambda i: i.split(" km/h")[0] if i != "-" else "")
 df["PriceinGermany"] = df["PriceinGermany"].str.extract(r"(\d.*)")
+df["PriceinGermany"] = df["PriceinGermany"].apply(lambda i: i.replace(",", "") if str(i) != "nan" else i)
+df["PriceinGermany"] = df["PriceinGermany"].apply(lambda i: int(i) if str(i) != "nan" else i)
 df["PriceinUK"] = df["PriceinUK"].str.extract(r"(\d.*)")
+df["PriceinUK"] = df["PriceinUK"].apply(lambda i: i.replace(",", "") if str(i) != "nan" else i)
+df["PriceinUK"] = df["PriceinUK"].apply(lambda i: int(i) if str(i) != "nan" else i)
 df["Brand"] = df["Name"].apply(lambda i: i.split(" ")[0])
+
+df["PriceinAUS"] = df["PriceinGermany"] * 1.6 
+df["PriceinAUS"] = df["PriceinUK"] * 1.87
 
 df.to_csv("clean_electricity_car.csv", index = False)
